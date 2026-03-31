b4bad55ff3e113eb07b029b5fe262de2cada547a
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-03-31 18:54:37 -0300

    feat: implemented a simple window test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+#include <SDL3/SDL.h>
 #include <iostream>
 
-auto main(int argc, char *argv[]) -> int {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        std::cout << "Failed to initialize SDL3" << std::endl;
+        return -1;
+    }
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    if (!SDL_CreateWindowAndRenderer(
+            "Game Engine", 800, 600,
+            SDL_WINDOW_BORDERLESS | SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        std::cout << "Failed to create a window and a renderer" << std::endl;
+    }
+
+    SDL_MaximizeWindow(window);
+
+    while (true) {
+        SDL_SetRenderDrawColor(renderer, 21, 21, 21, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
     return 0;
 }
